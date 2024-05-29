92e54d411a8627df9df1a988a9905bd50d5f94cc
Chen Li <138310520+ChenLi0620@users.noreply.github.com>
2024-05-29 23:39:39 +0800

    Update

--- eval/evals/eval_base.sh
+++ eval/evals/eval_base.sh
@@ -1,4 +1,4 @@
-OUTPUT_DIR=${1:-"/data/lc/guangzhou/ALMA/ALMA/ALMA/llama2_base/it/"}
+OUTPUT_DIR=${1:-""}
 TEST_PAIRS=${2:-"de-en"}
 export HF_DATASETS_CACHE=".cache/huggingface_cache/datasets"
 export TRANSFORMERS_CACHE=".cache/models/"
@@ -9,12 +9,12 @@ python \
     --do_predict \
     --low_cpu_mem_usage True \
     --language_pairs ${TEST_PAIRS} \
-    --mmt_data_path /data/lc/guangzhou/ALMA/ALMA/ALMA/human_written_data_domain_it \
-    --per_device_eval_batch_size 1 \
+    --mmt_data_path ./data/data/eval/it \
+    --per_device_eval_batch_size 2 \
     --output_dir ${OUTPUT_DIR} \
     --predict_with_generate \
     --max_new_tokens 256 \
-    --max_source_length 400 \
+    --max_source_length 512 \
     --fp16 \
     --seed 42 \
     --num_beams 5 \

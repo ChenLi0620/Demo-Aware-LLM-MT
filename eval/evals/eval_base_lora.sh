976a99145a97f235ed4978c0969fa0bcae827c90
Chen Li <138310520+ChenLi0620@users.noreply.github.com>
2024-05-29 23:38:40 +0800

    Update eval_base_lora.sh

--- eval/evals/eval_base_lora.sh
+++ eval/evals/eval_base_lora.sh
@@ -1,22 +1,22 @@
-OUTPUT_DIR=${1:-"/data/lc/guangzhou/ALMA/ALMA/ALMA/llama2_base/it/"}
+OUTPUT_DIR=${1:-""}
 TEST_PAIRS=${2:-"de-en"}
 export HF_DATASETS_CACHE=".cache/huggingface_cache/datasets"
 export TRANSFORMERS_CACHE=".cache/models/"
 #export CUDA_VISIBLE_DEVICES=1
 python \
     run_llmmt.py \
-    --model_name_or_path /data/llx/models--meta-llama--Llama-2-7b-hf/snapshots/6fdf2e60f86ff2481f2241aaee459f85b5b0bbb9 \
+    --model_name_or_path ./checkpoints/ALMA-7B \
     --do_predict \
     --low_cpu_mem_usage True \
     --language_pairs ${TEST_PAIRS} \
-    --mmt_data_path /data/lc/guangzhou/ALMA/ALMA/ALMA/human_written_data_domain_it \
+    --mmt_data_path ./data/data/eval/it \
     --per_device_eval_batch_size 1 \
     --use_peft \
-    --peft_model_id  /UNICOMFS/hitsz_mzhang_1/lc/ALMA/ALMA_lc/alma-7b-parallel-ft_lc_dp_context_r \
+    --peft_model_id  ./checkpoints/Demo_LoRA \
     --output_dir ${OUTPUT_DIR} \
     --predict_with_generate \
     --max_new_tokens 256 \
-    --max_source_length 400 \
+    --max_source_length 512 \
     --fp16 \
     --seed 42 \
     --num_beams 5 \
